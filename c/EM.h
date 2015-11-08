99f1852ba7fdb929b2877da8f9232b8eaf20968f
tclin <tclin914@gmail.com>
2015-11-08 14:24:34 +0800

    add macro to prevent repeat include header file

--- c/EM.h
+++ c/EM.h
@@ -1,2 +1,10 @@
+#ifndef EM_H
+#define EM_H
 
-void EM(int **train_images, int *train_labels, int **test_images, int *test_labels);
+#define K 40
+#define N 50000
+#define D 28 *28
+
+void EM(int **train_images, int *train_labels, int **test_images, int *test_labels, double mu[K][D], double *pi, double z[N][K]);
+
+#endif
