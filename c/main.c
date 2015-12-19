43a5b27126e6aa6d687956081c1ff47c4fdea66f
tclin <tclin914@gmail.com>
2015-12-20 03:07:18 +0800

    modify file path error

--- c/main.c
+++ c/main.c
@@ -5,7 +5,7 @@
 #include "EM.h"
 #include "loglikelihood.h"
 
-#define TRAIN_IMAGE "../data/data/train-images-idx3-ubyte"
+#define TRAIN_IMAGE "../data/train-images-idx3-ubyte"
 #define TRAIN_LABEL "../data/train-labels-idx1-ubyte"
 #define TEST_IMAGE "../data/t10k-images-idx3-ubyte"
 #define TEST_LABEL "../data/t10k-labels-idx1-ubyte"
